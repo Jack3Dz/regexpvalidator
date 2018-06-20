ff1f5ab8961b2e7cc3e36cd0517daf5806c587ee
Jefferson Damaceno <jefferson.damaceno17@gmail.com>
2018-06-19 23:32:43 -0300

    Ajuste

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  handleClick = () => {
+    console.log('teste');
+  };
+
   render() {
     return (
       <div className="App">
@@ -10,9 +14,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button onClick={this.handleClick}>
+          Validar
+        </button>
       </div>
     );
   }
